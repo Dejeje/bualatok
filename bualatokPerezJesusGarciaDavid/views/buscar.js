e8e0390862af8edc60a5211f2303c3e231915c0a
Jesus Perez <jesus.pereza@um.es>
2021-07-14 17:29:21 +0200

    .

--- bualatokPerezJesusGarciaDavid/views/buscar.js
+++ bualatokPerezJesusGarciaDavid/views/buscar.js
@@ -3,12 +3,19 @@ import { getAllProducts, comprarProducto } from '../src/Controller.js';
 document.addEventListener('DOMContentLoaded', async () => {
     var products = await getAllProducts();
     
-    
+    var i = 0;
     //guardar id hidden
     for (const product of products) {
         console.log(product);
-        var div = document.getElementById('container');
+        var container = document.getElementById('container');
         
+        var div = document.createElement('div');
+        
+        var hidden_id = document.createElement('label');
+        hidden_id.setAttribute('id', i);
+        hidden_id.setAttribute('value', product.id);
+        hidden_id.hidden = true;
+
         var nombre = document.createElement('label');
         nombre.setAttribute('class','productLabel');
         nombre.appendChild(document.createTextNode(product.name));
@@ -26,14 +33,19 @@ document.addEventListener('DOMContentLoaded', async () => {
 
         var comprar = document.createElement('input');
         comprar.setAttribute('class','productButton');
+        comprar.setAttribute('value', 'Comprar');
         comprar.setAttribute('type','button');
-        comprar.addEventListener('click', async function(product) {
-            await comprarProducto(product.id);
+        comprar.addEventListener('click', async function() {
+            console.log(document.getElementById(i.toString()).value);
         });
 
         div.appendChild(nombre);
         div.appendChild(description);
         div.appendChild(price);
-        div.appendChild(nombre); 
+        div.appendChild(comprar); 
+        
+        container.appendChild(div);
+
+        ++i;
     }
 });
