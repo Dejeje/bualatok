098d28fa8e60abc088949160cb1ebdc8f87c49f7
Jesus Perez <jesus.pereza@um.es>
2021-07-14 20:38:45 +0200

    You can now view your products

---
+++ bualatokPerezJesusGarciaDavid/views/tusProductos.js
@@ -0,0 +1,28 @@
+import { getAllProducts } from '../src/Controller.js';
+
+document.addEventListener('DOMContentLoaded', async () => {
+    var products = await getAllProducts();
+    for (const product of products) {
+        var container = document.getElementById('container');
+        
+        var div = document.createElement('div');
+        
+        var nombre = document.createElement('label');
+        nombre.setAttribute('class','productLabel');
+        nombre.appendChild(document.createTextNode(product.name));
+      
+        var description = document.createElement('label');
+        description.setAttribute('class','productTextArea');
+        description.appendChild(document.createTextNode(product.description));
+        
+        var price = document.createElement('label');
+        price.setAttribute('class','productPrice');
+        price.appendChild(document.createTextNode(product.price));
+
+        div.appendChild(nombre);
+        div.appendChild(description);
+        div.appendChild(price);
+        
+        container.appendChild(div);
+    }
+});
