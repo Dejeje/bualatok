10835e1f2649f69db70622a5a2243cc6e7b62ce1
Jesus Perez <jesus.pereza@um.es>
2021-07-14 12:50:28 +0200

    .

---
+++ bualatokPerezJesusGarciaDavid/views/menu.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('edit-user').addEventListener('click', function() {
+        window.location.replace('../public/editUsuario.html');
+    });
+    document.getElementById('registerProduct').addEventListener('click', function() {
+        window.location.replace('../public/registroProducto.html');
+    });
+    document.getElementById('searchProduct').addEventListener('click', function() {
+        window.location.replace('../public/buscar.html');
+    });
+    document.getElementById('products').addEventListener('click', function() {
+        window.location.replace('../public/editUsuario.html');
+    });
+    document.getElementById('logout').addEventListener('click', function() {
+        window.location.replace('../public/login.html');
+    });
+
+});
